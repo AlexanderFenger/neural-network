394fe9c37a720e7337b447601d20b3bad1adeef8
Alexander Fenger <alexander.fenger@rwth-aachen.de>
2022-10-10 21:04:57 +0200

    added comments
    added small comments for better understanding

--- sificc_lib/Event.py
+++ sificc_lib/Event.py
@@ -7,25 +7,40 @@ import numpy as np
 from sificc_lib import utils
 from uproot_methods.classes.TVector3 import TVector3
 
+
 class Event:
     '''Represents a single event in a ROOT file
     '''
-    
+
     # list of leaves that are required from a ROOT file to properly instantiate an Event object
-    l_leaves = ['Energy_Primary', 'RealEnergy_e', 'RealEnergy_p', 'RealPosition_source', 'SimulatedEventType',
-                'RealDirection_source', 'RealComptonPosition', 'RealDirection_scatter', 'RealPosition_e', 
-                'RealInteractions_e', 'RealPosition_p', 'RealInteractions_p', 'Identified', 'PurCrossed',
-                'RecoClusterPositions.position', 'RecoClusterPositions.uncertainty', 'RecoClusterEnergies', 
-                'RecoClusterEnergies.value', 'RecoClusterEnergies.uncertainty', 'RecoClusterEntries', 
-               ]
-    
-    def __init__(self, real_primary_energy, real_e_energy, real_p_energy, real_e_positions, 
-                 real_e_interactions, real_p_positions, real_p_interactions, real_src_pos, real_src_dir, 
-                 real_compton_pos, real_scatter_dir, identification_code, crossed, clusters_count, 
-                 clusters_position, clusters_position_unc, clusters_energy, clusters_energy_unc, 
+    l_leaves = ['Energy_Primary',
+                'RealEnergy_e',  # Gen: Energy electron
+                'RealEnergy_p',  # Gen:Energy proton
+                'RealPosition_source',  # Gen: position source
+                'SimulatedEventType',  # Gen: Event Tag
+                'RealDirection_source',
+                'RealComptonPosition',  # Gen: position of compton event (in scatterer?)
+                'RealDirection_scatter',
+                'RealPosition_e',
+                'RealInteractions_e',
+                'RealPosition_p',
+                'RealInteractions_p',
+                'Identified',
+                'PurCrossed',
+                'RecoClusterPositions.position',
+                'RecoClusterPositions.uncertainty',
+                'RecoClusterEnergies',
+                'RecoClusterEnergies.value',
+                'RecoClusterEnergies.uncertainty',
+                'RecoClusterEntries']
+
+    def __init__(self, real_primary_energy, real_e_energy, real_p_energy, real_e_positions,
+                 real_e_interactions, real_p_positions, real_p_interactions, real_src_pos, real_src_dir,
+                 real_compton_pos, real_scatter_dir, identification_code, crossed, clusters_count,
+                 clusters_position, clusters_position_unc, clusters_energy, clusters_energy_unc,
                  clusters_entries, event_type,
                  scatterer, absorber, clusters_limit
-                ):
+                 ):
         # define the main values of a simulated event
         self.event_type = event_type
         self.real_primary_energy = real_primary_energy
@@ -48,7 +63,7 @@ class Event:
         self.clusters_energy_unc = clusters_energy_unc
         self.clusters_entries = clusters_entries
         self.clusters_limit = clusters_limit
-        
+
         # check if the event is a valid event by considering the clusters associated with it
         # the event is considered valid if there are at least one cluster within each module of the SiFiCC
         if self.clusters_count >= 2 \
@@ -57,10 +72,10 @@ class Event:
             self.is_distributed_clusters = True
         else:
             self.is_distributed_clusters = False
-        
+
         # check if the event is a Compton event
         self.is_compton = True if self.real_e_energy != 0 else False
-        
+
         # check if the event is a complete Compton event
         # complete Compton event= Compton event + both e and p go through a second interation in which
         # 0 < p interaction < 10
@@ -71,24 +86,24 @@ class Event:
                 and len(self.real_e_position_all) >= 1 \
                 and ((self.real_p_interaction_all[1:] > 0) & (self.real_p_interaction_all[1:] < 10)).any() \
                 and ((self.real_e_interaction_all[0] >= 10) & (self.real_e_interaction_all[0] < 20)):
-            self.is_complete_compton = True 
+            self.is_complete_compton = True
         else:
             self.is_complete_compton = False
-        
+
         # initialize e & p first interaction position
         if self.is_complete_compton:
-            for idx in range(1,len(self.real_p_interaction_all)):
+            for idx in range(1, len(self.real_p_interaction_all)):
                 if 0 < self.real_p_interaction_all[idx] < 10:
                     self.real_p_position = self.real_p_position_all[idx]
                     break
-            for idx in range(0,len(self.real_e_interaction_all)):
+            for idx in range(0, len(self.real_e_interaction_all)):
                 if 10 <= self.real_e_interaction_all[idx] < 20:
                     self.real_e_position = self.real_e_position_all[idx]
                     break
         else:
-            self.real_p_position = TVector3(0,0,0)
-            self.real_e_position = TVector3(0,0,0)
-            
+            self.real_p_position = TVector3(0, 0, 0)
+            self.real_e_position = TVector3(0, 0, 0)
+
         # check if the event is a complete distributed Compton event
         # complete distributed Compton event= complete Compton event + each e and p go through a secondary 
         # interaction in a different module of the SiFiCC
@@ -98,7 +113,7 @@ class Event:
             self.is_complete_distributed_compton = True
         else:
             self.is_complete_distributed_compton = False
-            
+
         # check if the event is an ideal Compton event and what type is it (EP or PE)
         # ideal Compton event = complete distributed Compton event where the next interaction of both 
         # e and p is in the different modules of SiFiCC
@@ -120,14 +135,14 @@ class Event:
             self.is_ideal_compton = False
             self.is_ep = False
             self.is_pe = False
-        
+
     def _aggregate_max_clusters(self):
         '''Aggregate the top clusters in term of cluster energy within the `cluster_limit` (inplace sorting)'''
-        
+
         # redeclare clusters position to np array to be able to switch values within
         self.clusters_position = np.array(self.clusters_position)
         self.clusters_position_unc = np.array(self.clusters_position_unc)
-        
+
         # updates to the clusters only if their number is bigger than the limit
         if self.clusters_count > self.clusters_limit:
             # minimum energy of top clusters should be higher than the rest
@@ -137,7 +152,7 @@ class Event:
                 # and replace the clusters
                 min_pos = np.argmin(self.clusters_energy[:self.clusters_limit])
                 max_pos = np.argmax(self.clusters_energy[self.clusters_limit:]) + self.clusters_limit
-                
+
                 self.clusters_energy[min_pos], self.clusters_energy[max_pos] = \
                     self.clusters_energy[max_pos], self.clusters_energy[min_pos]
                 self.clusters_energy_unc[min_pos], self.clusters_energy_unc[max_pos] = \
@@ -148,7 +163,7 @@ class Event:
                     self.clusters_position_unc[max_pos], self.clusters_position_unc[min_pos]
                 self.clusters_entries[min_pos], self.clusters_entries[max_pos] = \
                     self.clusters_entries[max_pos], self.clusters_entries[min_pos]
-                
+
     def _sort_clusters(self):
         '''Sort event clusters in reverse based on their energy (inplace sorting)'''
         energy_sort = np.flip(np.argsort(self.clusters_energy))
@@ -157,7 +172,7 @@ class Event:
         self.clusters_position = self.clusters_position[energy_sort]
         self.clusters_position_unc = self.clusters_position_unc[energy_sort]
         self.clusters_entries = self.clusters_entries[energy_sort]
-        
+
     def _align_clusters(self):
         '''Align the cluster so that their count matches with `clusters_limit` either by trimming or padding'''
         # if cluster count is more than the limit then trim
@@ -167,83 +182,83 @@ class Event:
             self.clusters_position = self.clusters_position[:self.clusters_limit]
             self.clusters_position_unc = self.clusters_position_unc[:self.clusters_limit]
             self.clusters_entries = self.clusters_entries[:self.clusters_limit]
-            
+
         # otherwise, pad the clusters with the difference to reach `clusters_limit`
         else:
             padding = self.clusters_limit - self.clusters_count
-            
+
             self.clusters_energy = np.concatenate((self.clusters_energy, [0] * padding))
             self.clusters_energy_unc = np.concatenate((self.clusters_energy_unc, [0] * padding))
-            self.clusters_position = np.concatenate((self.clusters_position, [TVector3(0,0,0)] * padding))
-            self.clusters_position_unc = np.concatenate((self.clusters_position_unc, [TVector3(0,0,0)] * padding))
+            self.clusters_position = np.concatenate((self.clusters_position, [TVector3(0, 0, 0)] * padding))
+            self.clusters_position_unc = np.concatenate((self.clusters_position_unc, [TVector3(0, 0, 0)] * padding))
             self.clusters_entries = np.concatenate((self.clusters_entries, [0] * padding))
         # clusters count now is identical to clusters limit
         self.clusters_count = self.clusters_limit
-        
+
     @property
     def e_clusters_count(self):
         '''Calculates and returns the count of clusters matching with e position'''
         return self._count_e_clusters()
-    
+
     @property
     def p_clusters_count(self):
         '''Calculates and returns the count of clusters matching with p position'''
         return self._count_p_clusters()
-    
+
     @property
     def is_clusters_matching(self):
         '''Checks if only one cluster is assigned to each of e & p'''
         return True if self.e_clusters_count == 1 and self.p_clusters_count == 1 else False
-    
+
     @property
     def is_clusters_overlap(self):
         '''Checks if their are overlap of clusters assigned to e or p'''
         return True if self.e_clusters_count > 1 or self.p_clusters_count > 1 else False
-    
+
     def _count_e_clusters(self):
         '''Returns the count of clusters matching e position within their uncertainities'''
         return self._count_matching_clusters(self.real_e_position)
-    
+
     def _count_p_clusters(self):
         '''Returns the count of clusters matching p position within their uncertainities'''
         return self._count_matching_clusters(self.real_p_position)
-        
+
     def _count_matching_clusters(self, point):
         '''Returns the number of matching clusters to `point` within their uncertainities'''
         count = 0
-        
+
         for cluster, cluster_unc in zip(self.clusters_position, self.clusters_position_unc):
             if utils.is_point_inside_cluster(point, cluster, cluster_unc):
                 count += 1
         return count
-    
+
     def _arg_matching_cluster(self, point):
         '''Gets a point and returns the index of the first cluster matching with this point 
         within its uncertainities. Returns -1 when no cluster matches with `point`'''
-        
+
         for idx, (cluster, cluster_unc) in enumerate(zip(self.clusters_position, self.clusters_position_unc)):
             if utils.is_point_inside_cluster(point, cluster, cluster_unc):
                 return idx
         else:
             return -1
-            
+
     def _arg_closest_cluster(self, point):
         '''Gets a point a return the index of the closest cluster. Returns -1 when there are
         no clusters'''
         if self.clusters_count < 1:
             return -1
-        
+
         closest_idx = 0
         min_euclidean = utils.euclidean_distance(point, self.clusters_position[0])
-        
+
         for idx, cluster in enumerate(self.clusters_position):
             distance = utils.euclidean_distance(point, cluster)
             if distance < min_euclidean:
                 min_euclidean = distance
                 closest_idx = idx
-                
+
         return closest_idx
-    
+
     def get_features(self, sort=True):
         '''Generate and return the training features of the event. Features are of the form:
         [ cluster-1, cluster-2, ..., cluster-n ]
@@ -258,14 +273,14 @@ class Event:
         ]
         
         Output feature dimention is 1x(9*clusters_limit)'''
-        
+
         # sort the clusters and align the number of clusters to match `clusters_limit`
         if sort:
             self._sort_clusters()
         else:
             self._aggregate_max_clusters()
         self._align_clusters()
-        
+
         # build the list of clusters features
         # the abs is taken for position uncertainty because the y dimention is negative
         l_clusters = []
@@ -278,10 +293,10 @@ class Event:
                 np.abs(utils.vec_as_np(self.clusters_position_unc[i])),
             ))
             l_clusters.append(cluster)
-            
+
         features = np.concatenate(l_clusters)
         return features
-    
+
     def get_targets(self):
         '''Generates and return the event targets. 
         Targets are of dimension 1x11 and the format is:
@@ -298,7 +313,7 @@ class Event:
         # return the features only if the event is an ideal compton
         # otherwise return 0s
         if self.is_distributed_clusters and self.is_ideal_compton:
-            
+
             # find cluster index of both e & p
             if self.e_clusters_count == 1:
                 e_cluster_index = self._arg_matching_cluster(self.real_e_position)
@@ -308,7 +323,7 @@ class Event:
                 p_cluster_index = self._arg_matching_cluster(self.real_p_position)
             else:
                 p_cluster_index = self._arg_closest_cluster(self.real_p_position)
-                
+
             # build target features
             targets = np.concatenate((
                 [1],
@@ -321,5 +336,5 @@ class Event:
             ))
         else:
             targets = np.zeros(11)
-            
+
         return targets
--- sificc_lib/Simulation.py
+++ sificc_lib/Simulation.py
@@ -7,22 +7,27 @@ import uproot
 from sificc_lib import Event, SiFiCC_Module
 from tqdm import tqdm
 
+
 class Simulation:
     '''Process a ROOT simulation for SiFi-CC detection'''
+
+    # Preprocessing of Root files to event objects
+    # implementation of Root file->CSV file could be more beneficial
+
     def __init__(self, file_name, clusters_limit=6):
         root_file = uproot.open(file_name)
-        self.__setup(root_file)
+        self.__setup(root_file)  # why private?
         self.tree = root_file[b'Events']
         self.num_entries = self.tree.numentries
         self.clusters_limit = clusters_limit
-        
+
         self.clusters_count = self.tree['RecoClusterEnergies']
         self.clusters_position = self.tree['RecoClusterPositions.position']
         self.clusters_position_unc = self.tree['RecoClusterPositions.uncertainty']
         self.clusters_energy = self.tree['RecoClusterEnergies.value']
         self.clusters_energy_unc = self.tree['RecoClusterEnergies.uncertainty']
         self.clusters_entries = self.tree['RecoClusterEntries']
-            
+
     def __setup(self, root_file):
         '''Extract scatterer and absorber modules setup from the ROOT file'''
         setup = root_file[b'Setup']
@@ -30,14 +35,14 @@ class Simulation:
                                        setup['ScattererThickness_y'].array()[0],
                                        setup['ScattererThickness_z'].array()[0],
                                        setup['ScattererPosition'].array()[0],
-                                      )
+                                       )
         self.absorber = SiFiCC_Module(setup['AbsorberThickness_x'].array()[0],
-                                       setup['AbsorberThickness_y'].array()[0],
-                                       setup['AbsorberThickness_z'].array()[0],
-                                       setup['AbsorberPosition'].array()[0],
+                                      setup['AbsorberThickness_y'].array()[0],
+                                      setup['AbsorberThickness_z'].array()[0],
+                                      setup['AbsorberPosition'].array()[0],
                                       )
-        
-    def iterate_events(self, basket_size=100000, desc='processing root file', 
+
+    def iterate_events(self, basket_size=100000, desc='processing root file',
                        bar_update_size=1000, entrystart=None):
         '''Iterate throughout all the events within the ROOT file. 
         Returns an event object on each step.
@@ -45,53 +50,53 @@ class Simulation:
         total = self.num_entries if entrystart is None else self.num_entries - entrystart
         prog_bar = tqdm(total=total, ncols=100, file=sys.stdout, desc=desc)
         bar_step = 0
-        for start, end, basket in self.tree.iterate(Event.l_leaves, entrysteps=basket_size, 
+        for start, end, basket in self.tree.iterate(Event.l_leaves, entrysteps=basket_size,
                                                     reportentries=True, namedecode='utf-8',
                                                     entrystart=entrystart, entrystop=None):
-            length = end-start
+            length = end - start
             for idx in range(length):
                 yield self.__event_at_basket(basket, idx)
-                
+
                 bar_step += 1
                 if bar_step % bar_update_size == 0:
                     prog_bar.update(bar_update_size)
-                
+
         prog_bar.update(self.num_entries % bar_update_size)
         prog_bar.close()
-                
+
     def get_event(self, position):
         '''Return event object at a certain position within the ROOT file
         '''
-        for basket in self.tree.iterate(Event.l_leaves, entrystart=position, entrystop=position+1, 
+        for basket in self.tree.iterate(Event.l_leaves, entrystart=position, entrystop=position + 1,
                                         namedecode='utf-8'):
             return self.__event_at_basket(basket, 0)
-        
+
     def __event_at_basket(self, basket, position):
         '''Create and return event object at a certain position from a ROOT basket of data
         '''
-        
-        event = Event(real_primary_energy = basket['Energy_Primary'][position], 
-                      real_e_energy = basket['RealEnergy_e'][position], 
-                      real_p_energy = basket['RealEnergy_p'][position], 
-                      real_e_positions = basket['RealPosition_e'][position], 
-                      real_e_interactions = basket['RealInteractions_e'][position],
-                      real_p_positions = basket['RealPosition_p'][position], 
-                      real_p_interactions = basket['RealInteractions_p'][position],
-                      real_src_pos = basket['RealPosition_source'][position], 
-                      real_src_dir = basket['RealDirection_source'][position], 
-                      real_compton_pos = basket['RealComptonPosition'][position], 
-                      real_scatter_dir = basket['RealDirection_scatter'][position], 
-                      identification_code = basket['Identified'][position], 
-                      crossed = basket['PurCrossed'][position], 
-                      clusters_count = basket['RecoClusterEnergies'][position],
-                      clusters_position = basket['RecoClusterPositions.position'][position], 
-                      clusters_position_unc = basket['RecoClusterPositions.uncertainty'][position], 
-                      clusters_energy = basket['RecoClusterEnergies.value'][position], 
-                      clusters_energy_unc = basket['RecoClusterEnergies.uncertainty'][position], 
-                      clusters_entries = basket['RecoClusterEntries'][position],
-                      event_type = basket['SimulatedEventType'][position],
-                      scatterer = self.scatterer,
-                      absorber = self.absorber,
-                      clusters_limit = self.clusters_limit
-                     )
+
+        event = Event(real_primary_energy=basket['Energy_Primary'][position],
+                      real_e_energy=basket['RealEnergy_e'][position],
+                      real_p_energy=basket['RealEnergy_p'][position],
+                      real_e_positions=basket['RealPosition_e'][position],
+                      real_e_interactions=basket['RealInteractions_e'][position],
+                      real_p_positions=basket['RealPosition_p'][position],
+                      real_p_interactions=basket['RealInteractions_p'][position],
+                      real_src_pos=basket['RealPosition_source'][position],
+                      real_src_dir=basket['RealDirection_source'][position],
+                      real_compton_pos=basket['RealComptonPosition'][position],
+                      real_scatter_dir=basket['RealDirection_scatter'][position],
+                      identification_code=basket['Identified'][position],
+                      crossed=basket['PurCrossed'][position],
+                      clusters_count=basket['RecoClusterEnergies'][position],
+                      clusters_position=basket['RecoClusterPositions.position'][position],
+                      clusters_position_unc=basket['RecoClusterPositions.uncertainty'][position],
+                      clusters_energy=basket['RecoClusterEnergies.value'][position],
+                      clusters_energy_unc=basket['RecoClusterEnergies.uncertainty'][position],
+                      clusters_entries=basket['RecoClusterEntries'][position],
+                      event_type=basket['SimulatedEventType'][position],
+                      scatterer=self.scatterer,
+                      absorber=self.absorber,
+                      clusters_limit=self.clusters_limit
+                      )
         return event
