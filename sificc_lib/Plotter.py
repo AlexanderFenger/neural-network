b12e6c5f4e1da3522d3593c0b59023a704b2c28c
AlexanderFenger <alexander.fenger@rwth-aachen.de>
2022-10-17 17:11:47 +0200

    small fixes

--- sificc_lib/Plotter.py
+++ sificc_lib/Plotter.py
@@ -1,23 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import Preprocessing
 
 
 class Plotter:
     def __init__(self, dir_plot):
         self.dir_plot = dir_plot  # the target directory for plot PDFs
 
-    def plot_source_dist(self, preprocessing: Preprocessing, filename):
-        """plot the source distribution as a 2D scatter plot"""
-
-        list_x = []
-        list_y = []
-
-        for event in preprocessing.iterate_events(n=10000):
-            list_x.append(event.RealPosition_source.x)
-            list_y.append(event.RealPosition_source.y)
-
-        # Scatter plot
-        plt.figure()
-        plt.hist2d(list_x, list_y)
-        plt.savefig(self.dir_plot + filename + ".pdf")
