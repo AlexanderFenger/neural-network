7785a86bf854cfdfda66aabc87b3789beb61f745
AlexanderFenger <alexander.fenger@rwth-aachen.de>
2022-10-17 14:20:20 +0200

    Update testfile.py
    modified testfile for plotting test

--- testfile.py
+++ testfile.py
@@ -1,9 +1,15 @@
 import numpy as np
+import os
 
 from sificc_lib.Preprocessing import Preprocessing
 from sificc_lib.utilities import utilities
 from sificc_lib.root_files import root_files
+from sificc_lib.Plotter import Plotter
 
 preprocessing = Preprocessing(root_files.root_test)
 print("loaded file: ", root_files.root_test)
 utilities.print_event_summary(preprocessing)
+
+dir_main = os.getcwd()
+plotter = Plotter(dir_main + "/plots/")
+plotter.plot_source_dist(preprocessing, "hist2d_test")
