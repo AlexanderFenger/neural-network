12520d1d872d20aff1080f7c3e0ab1beb89d0994
AlexanderFenger <alexander.fenger@rwth-aachen.de>
2022-10-26 14:01:45 +0200

    Update utilities.py
    updated macro for new dataset

--- sificc_lib/utilities.py
+++ sificc_lib/utilities.py
@@ -3,7 +3,6 @@ import numpy as np
 
 class utilities:
 
-
     def print_event_summary(preprocessing, n=0):
         """Prints out a summary of one random event"""
 
@@ -12,43 +11,42 @@ class utilities:
 
         print("\nPrinting event summary\n")
         print("Event number: ", n)
-        print("Event type: {}".format(event.SimulatedEventType))
+        print("Event type: {}".format(event.MCSimulatedEventType))
         print("--------------")
-        print("EnergyPrimary: {:.3f}".format(event.Energy_Primary))
-        print("RealEnergy_e: {:.3f}".format(event.RealEnergy_e))
-        print("RealEnergy_p: {:.3f}".format(event.RealEnergy_p))
-        print("RealPosition_source: ({:7.3f}, {:7.3f}, {:7.3f})".format(event.RealPosition_source.x,
-                                                                        event.RealPosition_source.y,
-                                                                        event.RealPosition_source.z))
-        print("RealDirection_source: ({:7.3f}, {:7.3f}, {:7.3f})".format(event.RealDirection_source.x,
-                                                                         event.RealDirection_source.y,
-                                                                         event.RealDirection_source.z))
-        print("RealComptonPosition: ({:7.3f}, {:7.3f}, {:7.3f})".format(event.RealComptonPosition.x,
-                                                                        event.RealComptonPosition.y,
-                                                                        event.RealComptonPosition.z))
-        print("RealDirection_scatter: ({:7.3f}, {:7.3f}, {:7.3f})".format(event.RealDirection_scatter.x,
-                                                                          event.RealDirection_scatter.y,
-                                                                          event.RealDirection_scatter.z))
+        print("EnergyPrimary: {:.3f}".format(event.MCEnergy_Primary))
+        print("RealEnergy_e: {:.3f}".format(event.MCEnergy_e))
+        print("RealEnergy_p: {:.3f}".format(event.MCEnergy_p))
+        print("RealPosition_source: ({:7.3f}, {:7.3f}, {:7.3f})".format(event.MCPosition_source.x,
+                                                                        event.MCPosition_source.y,
+                                                                        event.MCPosition_source.z))
+        print("RealDirection_source: ({:7.3f}, {:7.3f}, {:7.3f})".format(event.MCDirection_source.x,
+                                                                         event.MCDirection_source.y,
+                                                                         event.MCDirection_source.z))
+        print("RealComptonPosition: ({:7.3f}, {:7.3f}, {:7.3f})".format(event.MCComptonPosition.x,
+                                                                        event.MCComptonPosition.y,
+                                                                        event.MCComptonPosition.z))
+        print("RealDirection_scatter: ({:7.3f}, {:7.3f}, {:7.3f})".format(event.MCDirection_scatter.x,
+                                                                          event.MCDirection_scatter.y,
+                                                                          event.MCDirection_scatter.z))
         print("\nRealPosition_e / RealInteractions_e:")
-        for i in range(len(event.RealInteractions_e)):
-            print("({:7.3f}, {:7.3f}, {:7.3f}) | {}".format(event.RealPosition_e.x[i],
-                                                            event.RealPosition_e.y[i],
-                                                            event.RealPosition_e.z[i],
-                                                            event.RealInteractions_e[i]))
+        for i in range(len(event.MCInteractions_e)):
+            print("({:7.3f}, {:7.3f}, {:7.3f}) | {}".format(event.MCPosition_e.x[i],
+                                                            event.MCPosition_e.y[i],
+                                                            event.MCPosition_e.z[i],
+                                                            event.MCInteractions_e[i]))
         print("\nRealPosition_p / RealInteractions_p:")
-        for i in range(len(event.RealInteractions_p)):
-            print("({:7.3f}, {:7.3f}, {:7.3f}) | {}".format(event.RealPosition_p.x[i],
-                                                            event.RealPosition_p.y[i],
-                                                            event.RealPosition_p.z[i],
-                                                            event.RealInteractions_p[i]))
+        for i in range(len(event.MCInteractions_p)):
+            print("({:7.3f}, {:7.3f}, {:7.3f}) | {}".format(event.MCPosition_p.x[i],
+                                                            event.MCPosition_p.y[i],
+                                                            event.MCPosition_p.z[i],
+                                                            event.MCInteractions_p[i]))
 
         print("\n Cluster Entries: ")
         print("Energy / Position / Entries / Module")
         for i, cluster in enumerate(event.RecoClusterPosition):
-            print("{:.3f} | {} | ({:7.3f}, {:7.3f}, {:7.3f}) | {:3} | Module: {}".format(i,
-                                                                                         event.RecoClusterEnergies_values[i],
-                                                                                         cluster.x,
-                                                                                         cluster.y,
-                                                                                         cluster.z,
-                                                                                         event.RecoClusterEntries[i],
-                                                                                         event.cluster_module(cluster)))
+            print("{:.3f} | {} | ({:7.3f}, {:7.3f}, {:7.3f}) | {:3} ".format(i,
+                                                                             event.RecoClusterEnergies_values[i],
+                                                                             cluster.x,
+                                                                             cluster.y,
+                                                                             cluster.z,
+                                                                             event.RecoClusterEntries[i]))
