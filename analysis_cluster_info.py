4e67d8a2390238c9746f39eeb6aa79586dad26a9
AlexanderFenger <alexander.fenger@rwth-aachen.de>
2022-10-26 14:01:11 +0200

    Update analysis_cluster_info.py
    finished analysis

--- analysis_cluster_info.py
+++ analysis_cluster_info.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 from sificc_lib import Preprocessing
 from sificc_lib.root_files import root_files
+from sificc_lib import utilities
 from uproot_methods.classes.TVector3 import TVector3
 
 ########################################################################################################################
@@ -18,6 +19,9 @@ preprocessing_0mm = Preprocessing(dir_main + root_files.optimized_0mm_local)
 preprocessing_5mm = Preprocessing(dir_main + root_files.optimized_5mm_local)
 print("loaded file: ", root_files.optimized_0mm_local)
 print("loaded file: ", root_files.optimized_5mm_local)
+
+
+# utilities.print_event_summary(preprocessing_0mm, 30)
 
 
 def process_data(preprocessing: Preprocessing):
@@ -65,21 +69,25 @@ def process_data(preprocessing: Preprocessing):
         # Cut-Based selection tag
         event_data.append(event.Identified * 1)
 
-        # cluster ids of the highest energy cluster
-        event_data.append(idx_scatterer[0])
-        event_data.append(idx_absorber[0])
+        # compare the highest energy cluster in scatterer and absorber and only
+        # count the higher one
+        event_data.append((idx_scatterer[0] - len(idx_scatterer) + 1) * (-1))
+        event_data.append((idx_absorber[0] - len(idx_absorber) + 1) * (-1))
 
         # argmatch clustering for electron and photon
         if event.is_compton_ideal:
             event_data.append(event.argmatch_cluster(event.MCPosition_e_first))
             event_data.append(event.argmatch_cluster(event.MCPosition_p_first))
+            # correct scatterer indexing
+            if event_data[8] != -1:
+                event_data[8] = (event_data[8] - (len(idx_scatterer) + len(idx_absorber)) + 1) * (-1)
 
-            # correct absorber indexing by ignoring the scatterer idxs
+            # correct absorber indexing
             if event_data[9] != -1:
-                event_data[9] -= len(idx_scatterer)
+                event_data[9] = (event_data[9] - len(idx_absorber) + 1) * (-1)
         else:
-            event_data.append(-1)
-            event_data.append(-1)
+            event_data.append(-2)
+            event_data.append(-2)
 
         # end
         data.append(event_data)
@@ -100,8 +108,7 @@ n_0mm = np.sum(data_0mm[:, 1])
 n_5mm = np.sum(data_5mm[:, 1])
 
 ###########################################################################
-# plot distribution number of cluster per event split in scatterer/absorber
-# for total, CB
+# plot distribution number of cluster total
 hist_0mm_total, _ = np.histogram(data_0mm[:, 1], bins=bins_cluster)
 hist_5mm_total, _ = np.histogram(data_5mm[:, 1], bins=bins_cluster)
 
@@ -118,41 +125,31 @@ plt.bar(bins_cluster[:-1] + 0.70, hist_5mm_total / np.sum(n_5mm), fill=True, wid
 plt.legend()
 plt.show()
 
-#############################################################
-# plot distribution number of cluster per event for scatterer
+##########################################################################
+# plot distribution number of cluster per event for scatterer and absorber
+bins_cluster = np.arange(-0.5, 10.5, 1.0)
 hist_0mm_scatterer, _ = np.histogram(data_0mm[:, 2], bins=bins_cluster)
 hist_0mm_absorber, _ = np.histogram(data_0mm[:, 3], bins=bins_cluster)
+
+hist_0mm_scatterer_ic, _ = np.histogram([data_0mm[i, 2] for i in range(len(data_0mm[:, 2])) if data_0mm[i, 4] == 1],
+                                        bins=bins_cluster)
+hist_0mm_absorber_ic, _ = np.histogram([data_0mm[i, 3] for i in range(len(data_0mm[:, 3])) if data_0mm[i, 4] == 1],
+                                       bins=bins_cluster)
 
 plt.figure()
 plt.title("Distribution cluster per module (0mm)")
 plt.xlabel("# of clusters")
-plt.ylabel("counts (normalized)")
-plt.xlim(-0.5, 15)
+plt.ylabel("counts (normalized per module)")
+plt.xlim(-0.5, 12)
 plt.xticks(bins_cluster + 0.5)
 plt.bar(bins_cluster[:-1] + 0.30, hist_0mm_scatterer / np.sum(hist_0mm_scatterer), width=0.4, align="center",
-        color="blue", alpha=1.0, label="scatterer")
+        color="blue", alpha=0.8, label="scatterer")
 plt.bar(bins_cluster[:-1] + 0.70, hist_0mm_absorber / np.sum(hist_0mm_absorber), width=0.4, align="center",
-        color="orange", alpha=1.0, label="absorber")
-plt.legend()
-plt.show()
-
-###########################################################################
-# plot distribution number of cluster per ideal compton event for scatterer
-hist_0mm_scatterer, _ = np.histogram([data_0mm[i, 2] for i in range(len(data_0mm[:, 2])) if data_0mm[i, 4] == 1],
-                                     bins=bins_cluster)
-hist_0mm_absorber, _ = np.histogram([data_0mm[i, 3] for i in range(len(data_0mm[:, 3])) if data_0mm[i, 4] == 1],
-                                    bins=bins_cluster)
-
-plt.figure()
-plt.title("Distribution cluster per module (0mm, ideal compton)")
-plt.xlabel("# of clusters")
-plt.ylabel("counts (normalized)")
-plt.xlim(-0.5, 15)
-plt.xticks(bins_cluster + 0.5)
-plt.bar(bins_cluster[:-1] + 0.30, hist_0mm_scatterer / np.sum(hist_0mm_scatterer), width=0.4, align="center",
-        color="blue", alpha=1.0, label="scatterer")
-plt.bar(bins_cluster[:-1] + 0.70, hist_0mm_absorber / np.sum(hist_0mm_absorber), width=0.4, align="center",
-        color="orange", alpha=1.0, label="absorber")
+        color="orange", alpha=0.8, label="absorber")
+plt.plot(bins_cluster[1:-1] + 0.30, hist_0mm_scatterer_ic[1:] / np.sum(hist_0mm_scatterer_ic), color="darkblue",
+         linestyle="-.", label="scatterer ideal compton", marker="x")
+plt.plot(bins_cluster[1:-1] + 0.70, hist_0mm_absorber_ic[1:] / np.sum(hist_0mm_absorber_ic), color="darkorange",
+         linestyle="--", label="absorber ideal compton", marker="o")
 plt.legend()
 plt.show()
 
@@ -166,8 +163,8 @@ hist_0mm_absorber, _ = np.histogram([data_0mm[i, 3] for i in range(len(data_0mm[
 plt.figure()
 plt.title("Distribution cluster per module (0mm, CB-selected)")
 plt.xlabel("# of clusters")
-plt.ylabel("counts (normalized)")
-plt.xlim(-0.5, 15)
+plt.ylabel("counts (normalized per module)")
+plt.xlim(-0.5, 11)
 plt.xticks(bins_cluster + 0.5)
 plt.bar(bins_cluster[:-1] + 0.30, hist_0mm_scatterer / np.sum(hist_0mm_scatterer), width=0.4, align="center",
         color="blue", alpha=1.0, label="scatterer")
@@ -178,22 +175,33 @@ plt.show()
 
 ###########################################################################
 # plot distribution cluster idx with the highest energy
+# competitive for both modules
 hist_0mm_scatterer, _ = np.histogram(data_0mm[:, 6], bins=bins_cluster)
 hist_0mm_absorber, _ = np.histogram(data_0mm[:, 7], bins=bins_cluster)
+
+hist_0mm_scatterer_ic, _ = np.histogram([data_0mm[i, 6] for i in range(len(data_0mm[:, 6])) if data_0mm[i, 4] != 0],
+                                        bins=bins_cluster)
+hist_0mm_absorber_ic, _ = np.histogram([data_0mm[i, 7] for i in range(len(data_0mm[:, 7])) if data_0mm[i, 4] != 0],
+                                       bins=bins_cluster)
 
 plt.figure()
 plt.title("Distribution highest energy cluster (0mm)")
 plt.xlabel("cluster idx")
-plt.ylabel("counts (normalized)")
-plt.xlim(-0.5, 15)
+plt.ylabel("counts (normalized per module)")
+plt.xlim(-0.5, 10.5)
 plt.xticks(bins_cluster + 0.5)
-plt.bar(bins_cluster[:-1] + 0.30, hist_0mm_scatterer / np.sum(hist_0mm_scatterer), width=0.4, align="center",
-        color="blue", alpha=1.0, label="scatterer")
-plt.bar(bins_cluster[:-1] + 0.70, hist_0mm_absorber / np.sum(hist_0mm_absorber), width=0.4, align="center",
-        color="orange", alpha=1.0, label="absorber")
+plt.bar(bins_cluster[:-1] + 0.30, hist_0mm_scatterer / (np.sum(hist_0mm_scatterer)),
+        width=0.4, align="center", color="blue", alpha=0.8, label="scatterer")
+plt.bar(bins_cluster[:-1] + 0.70, hist_0mm_absorber / (np.sum(hist_0mm_absorber)),
+        width=0.4, align="center", color="orange", alpha=0.8, label="absorber")
+plt.plot(bins_cluster[:-1] + 0.30,
+         hist_0mm_scatterer_ic / (np.sum(hist_0mm_scatterer_ic))
+         , color="darkblue", linestyle="-.", label="scatterer ideal compton", marker="x")
+plt.plot(bins_cluster[:-1] + 0.70,
+         hist_0mm_absorber_ic / (np.sum(hist_0mm_absorber_ic))
+         , color="darkorange", linestyle="--", label="absorber ideal compton", marker="o")
 plt.legend()
 plt.show()
-
 
 ###########################################################################
 # plot distribution cluster idx matching e and p position
@@ -202,7 +210,7 @@ hist_0mm_e_argmatch, _ = np.histogram(data_0mm[:, 8], bins=bins_argmatch)
 hist_0mm_p_argmatch, _ = np.histogram(data_0mm[:, 9], bins=bins_argmatch)
 
 plt.figure()
-plt.title("Distribution argmatch (0mm)")
+plt.title("Distribution argmatch (0mm, ideal compton)")
 plt.xlabel("cluster idx")
 plt.ylabel("counts (normalized)")
 plt.xlim(-1.5, 10)
@@ -210,6 +218,6 @@ plt.xticks(bins_argmatch + 0.5)
 plt.bar(bins_argmatch[:-1] + 0.30, hist_0mm_e_argmatch / np.sum(hist_0mm_e_argmatch), width=0.4, align="center",
         color="blue", alpha=1.0, label="e pos")
 plt.bar(bins_argmatch[:-1] + 0.70, hist_0mm_p_argmatch / np.sum(hist_0mm_p_argmatch), width=0.4, align="center",
-        color="orange", alpha=1.0, label="p pos (corrected)")
+        color="orange", alpha=1.0, label="p pos")
 plt.legend()
 plt.show()
